bf79618da8e44b777cefe6f1d361009cbfb65eef
kei <kei.khcheung@gmail.com>
2021-05-08 20:26:19 +0200

    markdown-it with smart quotes

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,10 @@ const htmlmin = require('html-minifier');
 const dateFns = require('date-fns');
 const lazyImagesPlugin = require('eleventy-plugin-lazyimages');
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+const markdownIt = require('markdown-it');
+const markdownItAnchor = require('markdown-it-anchor');
+// const markdownItFootnote = require("markdown-it-footnote");
+// const markdownItAttrs = require("markdown-it-attrs");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(syntaxHighlight);
@@ -41,6 +45,30 @@ module.exports = function (eleventyConfig) {
 
     return content;
   });
+
+  /* Markdown Overrides */
+  let markdownLibrary = markdownIt({
+    html: true,
+    breaks: true,
+    linkify: true,
+    typographer: true,
+  }).use(markdownItAnchor, {
+    permalink: true,
+    permalinkBefore: true,
+    permalinkSymbol: '',
+  }); /*.use(function(md) {
+    // Recognize Mediawiki links ([[text]])
+    md.linkify.add("[[", {
+      validate: /^\s?([^\[\]\|\n\r]+)(\|[^\[\]\|\n\r]+)?\s?\]\]/,
+      normalize: match => {
+        const parts = match.raw.slice(2,-2).split("|");
+        parts[0] = parts[0].replace(/.(md|markdown)\s?$/i, "");
+        match.text = (parts[1] || parts[0]).trim();
+        match.url = `/posts/${parts[0].trim()}/`;
+      }
+    })
+  })*/
+  eleventyConfig.setLibrary('md', markdownLibrary);
 
   return {
     dir: { input: 'src', output: '_site', data: '_data' },
